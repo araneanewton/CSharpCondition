bc4bb9a13372c2ff4e95e78cc03176f486af4180
araneanewton <araneanewton@gmail.com>
2025-05-13 21:36:02 +0700

    sample if else

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+public class Program 
+    {
+    
+        private static async Task Main(string[] args)
+        {
+            int a=10;
+            if (a==0)
+            {
+                Console.WriteLine("Ahihi");
+            }
+            else
+            {
+                Console.WriteLine("uhuhu");
+            }
+        }
+    }      
